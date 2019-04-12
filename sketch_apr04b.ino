899a3fb0d50e77d1edaa2c9b4cb4591b546f8629
Jake <monkeypsycho03@gmail.com>
2019-04-12 11:49:59 -0600

    Add files via upload

--- sketch_apr04b.ino
+++ sketch_apr04b.ino
@@ -1,33 +1,44 @@
+#include <IRremote.h>
+
+int RECV_PIN = 2;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
 const int controlPin1 = 11;
 const int controlPin2 = 3;
 const int enablePin = 5;
 const int irPin = 2;
 
+bool isMotorOn = false;
 
 void setup() {
   pinMode(controlPin1, OUTPUT);
   pinMode(controlPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
   
-  digitalWrite(controlPin1, LOW);
-  digitalWrite(controlPin2, HIGH);
-  
-  digitalWrite(enablePin, HIGH);
-  
   Serial.begin(9600);
-  delay(2000); while (!Serial); //delay for Leonardo
+  
+  irrecv.enableIRIn();
 
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    bool incomingByte = Serial.read() - 48;
-    if(incomingByte){
-      Serial.println("works");
+  if (irrecv.decode(&results)) {
+    if(results.value == 0x511DBB){
+      motorMove(true);
     }
-    Serial.println(incomingByte);
-    motorMove(incomingByte);
+    if(results.value == 0xA3C8EDDB){
+      motorMove(false);
+    }
+    if(results.value == 0xE318261B){
+      motorState();
+    }
+    irrecv.resume(); // Receive the next value
   }
+  analogWrite(enablePin, isMotorOn);
+  Serial.println(isMotorOn);
 }
 
 void motorMove(bool motorDirection){
@@ -41,3 +52,18 @@ void motorMove(bool motorDirection){
     digitalWrite(controlPin2, LOW);
   }
 }
+
+void motorState(){
+  isMotorOn = !isMotorOn;
+}
+/* up down left right power
+511DBB
+A3C8EDDB
+52A3D41F
+20FE4DBB
+E318261B
+
+ * 
+ * 
+ * /
+ */
